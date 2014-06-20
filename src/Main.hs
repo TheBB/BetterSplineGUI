bf6cac9e9c0a647276f2e6c1f7edea9ac61ae9c3
Eivind Fonn <evfonn@gmail.com>
2014-06-20 14:47:01 +0200

    Tutorial 5

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
+import qualified Data.ByteString as BS
 import Control.Applicative
+import Data.FileEmbed (embedFile)
 import System.FilePath ((</>))
   
+import qualified Data.Vinyl as V
+import qualified Graphics.GLUtil as U
+import qualified Graphics.GLUtil.Camera3D as U
 import qualified Graphics.Rendering.OpenGL as GL
 import qualified Graphics.UI.GLFW as GLFW
-import Graphics.GLUtil as U
+import qualified Graphics.VinylGL as VG
+import qualified Linear as L
+import Data.Vinyl ((:::), (=:), (<+>))
 import Graphics.Rendering.OpenGL (($=))
-import Linear as L
 
 import qualified Util.GLFW as W
 
-  
+ 
 main :: IO ()
 main = do
   win <- W.initialize "My First Triangle"
@@ -21,75 +30,83 @@ main = do
   W.mainLoop (draw program win) win
   W.cleanup win
 
-  
+vSrc = $(embedFile "src/shader/vs.glsl") :: BS.ByteString
+fSrc = $(embedFile "src/shader/fs.glsl") :: BS.ByteString
+ 
 initResources :: IO Resources
 initResources = do
   GL.blend $= GL.Enabled
   GL.blendFunc $= (GL.SrcAlpha, GL.OneMinusSrcAlpha)
-  let v = shaderPath </> "vs.glsl"
-      f = shaderPath </> "fs.glsl"
   Resources
-    <$> U.simpleShaderProgram v f
-    <*> U.makeBuffer GL.ArrayBuffer vertices
-    <*> U.makeBuffer GL.ArrayBuffer colors
+    <$> U.simpleShaderProgramBS vSrc fSrc
+    <*> VG.bufferVertices (zipWith (<+>) vertices colors)
+    <*> U.fromSource GL.ElementArrayBuffer elements
 
-  
+ 
 draw :: Resources -> GLFW.Window -> IO ()
-draw r win = do
+draw (Resources s vb e) win = do
   GL.clearColor $= GL.Color4 1 1 1 1
-  GL.clear [GL.ColorBuffer]
-  
+  GL.depthFunc $= Just GL.Less
+  GL.clear [GL.ColorBuffer, GL.DepthBuffer]
+ 
   (width, height) <- GLFW.getFramebufferSize win
   GL.viewport $= (GL.Position 0 0, GL.Size (fromIntegral width) (fromIntegral height))
 
   t <- maybe 0 id <$> GLFW.getTime
 
-  GL.currentProgram $= (Just . U.program . triProgram $ r)
-  U.enableAttrib (triProgram r) "coord3d"
-  U.enableAttrib (triProgram r) "v_color"
-  
-  GL.bindBuffer GL.ArrayBuffer $= Just (vertBuffer r)
-  U.setAttrib (triProgram r) "coord3d" GL.ToFloat $
-    GL.VertexArrayDescriptor 2 GL.Float 0 U.offset0
-  
-  GL.bindBuffer GL.ArrayBuffer $= Just (colorBuffer r)
-  U.setAttrib (triProgram r) "v_color" GL.ToFloat $
-    GL.VertexArrayDescriptor 3 GL.Float 0 U.offset0
-  
-  U.asUniform (transformM t) $ U.getUniform (triProgram r) "m_transform"
-  
-  GL.drawArrays GL.Triangles 0 3
-  
-  GL.vertexAttribArray (U.getAttrib (triProgram r) "coord3d") $= GL.Disabled
-  GL.vertexAttribArray (U.getAttrib (triProgram r) "v_color") $= GL.Disabled
+  GL.currentProgram $= (Just . U.program $ s)
+  VG.enableVertices' s vb
+  VG.bindVertices vb
+  VG.setAllUniforms s $ transformM width height t
+  GL.bindBuffer GL.ElementArrayBuffer $= Just e
+  U.drawIndexedTris (fromIntegral $ length elements)
 
 
 data Resources = Resources
-                 { triProgram :: U.ShaderProgram
-                 , vertBuffer :: GL.BufferObject
-                 , colorBuffer :: GL.BufferObject
+                 { shaderProgram :: U.ShaderProgram
+                 , buffer :: VG.BufferedVertices [Pos, Color]
+                 , elementBuffer :: GL.BufferObject
                  }
 
-transformM :: Real n => n -> L.M44 GL.GLfloat
-transformM t' = translation L.!*! rotation where
-  translation = L.mkTransformationMat L.eye3 $ L.V3 dx 0 0
-  rotation = L.m33_to_m44 . L.fromQuaternion $ L.axisAngle (L.V3 0 0 1) angle
-  dx = sin t * 2 * pi / 5
-  angle = t * pi / 4
-  t = realToFrac t'
+transformM :: Int -> Int -> Double -> V.PlainRec '[MVP]
+transformM width height t = mvp =: (proj L.!*! view L.!*! model L.!*! anim)
+  where anim  = L.mkTransformation (L.axisAngle (L.V3 0 1 0) angle) L.zero
+        model = L.mkTransformationMat L.eye3 $ L.V3 0 0 (-4)
+        view  = U.camMatrix $ U.tilt (-30) . U.dolly (L.V3 0 2 0) $ U.fpsCamera
+        proj  = U.projectionMatrix (pi/4) aspect 0.1 10
+
+        angle = realToFrac t * pi / 4
+        aspect = fromIntegral width / fromIntegral height
  
 
 shaderPath :: FilePath
 shaderPath = "src" </> "shader"
 
-vertices :: [L.V2 Float]
-vertices = [ L.V2 0.0 0.8
-           , L.V2 (-0.8) (-0.8)
-           , L.V2 0.8 (-0.8)
+vertices :: [V.PlainRec '[Pos]]
+vertices = map (coord3d =:) $ L.V3 <$> [1,-1] <*> [1,-1] <*> [1,-1]
+
+colors :: [V.PlainRec '[Color]]
+colors = map (v_color =:) $ L.V3 <$> [1,-1] <*> [1,-1] <*> [1,-1]
+
+elements :: [L.V3 GL.GLuint]
+elements = [ L.V3 2 1 0
+           , L.V3 1 2 3
+           , L.V3 0 1 4
+           , L.V3 4 1 5
+           , L.V3 4 5 6
+           , L.V3 7 6 5
+           , L.V3 2 6 3
+           , L.V3 6 3 7
+           , L.V3 0 4 2
+           , L.V3 2 4 6
+           , L.V3 5 1 7
+           , L.V3 7 1 3
            ]
 
-colors :: [L.V3 Float]
-colors = [ L.V3 1 1 0
-         , L.V3 0 0 1
-         , L.V3 1 0 0
-         ]
+type Pos = "coord3d" ::: L.V3 GL.GLfloat
+type Color = "v_color" ::: L.V3 GL.GLfloat
+type MVP = "mvp" ::: L.M44 GL.GLfloat
+
+coord3d = V.Field :: Pos
+v_color = V.Field :: Color
+mvp = V.Field :: MVP
